3e7adeacd46337428adad92a3becef6768f60bd4
h1ghl1kh7 <kimhyilim.915@gmail.com>
2023-01-16 00:11:55 +0900

    nvim + vim vim-plug

--- ubuntu/zsh-plugin.sh
+++ ubuntu/zsh-plugin.sh
@@ -12,6 +12,9 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 cd $installer_path
 
 cp .p10k.zsh ~/
